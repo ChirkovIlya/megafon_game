a18f00ef20814a0692fd08a93c36ea98928c4cfa
Morder Design <ilya72net@gmail.com>
2018-11-17 02:16:36 +0500

    prestory gameplay

--- script.js
+++ script.js
@@ -1,38 +1,12 @@
-var $el = $("#very-specific-design");
-var elHeight = $el.outerHeight();
-var elWidth = $el.outerWidth();
-//
-// var $wrapper = $(window);
-//
-// $wrapper.resizable({
-//   resize: doResize
-// });
-//
-// function doResize(event, ui) {
-//
-//   var scale, origin;
-//
-//   scale = Math.min(
-//     ui.size.width / elWidth,
-//     ui.size.height / elHeight
-//   );
-//
-//   $el.css({
-//     transform: "translate(-50%, -50%) " + "scale(" + scale + ")"
-//   });
-//
-// }
-//
-// var starterData = {
-//   size: {
-//     width: $wrapper.width(),
-//     height: $wrapper.height()
-//   }
-// }
-// doResize(null, starterData);
+let $el = $("#game-container");
+let elHeight = $el.outerHeight();
+let elWidth = $el.outerWidth();
+let prestoryJson = {};
+
 $( document ).ready(function() {
     console.log( "ready!" );
     initResizeGameArea();
+    initPrestory();
 });
 
 $( window ).resize(function() {
@@ -40,7 +14,7 @@ $( window ).resize(function() {
 });
 
 function initResizeGameArea() {
-  var scale, origin;
+  let scale, origin;
 
   scale = Math.min(
     $( window ).width() / elWidth,
@@ -50,3 +24,70 @@ function initResizeGameArea() {
     transform: "translate(-50%, -50%) " + "scale(" + scale + ")"
   });
 };
+
+function initPrestory(){
+  $.getJSON( "json/prestory.json", function( json ) {
+    console.log(json.prestory);
+    $.each(json.prestory, function(i, v){
+      let html;
+      if(i == 0){
+        let nextSlide = i+1;
+        html = `<div class="history-slide active" id="history-`+i+`">
+                  <div class="history-image" style="background-image: url('весна.jpg'); background-size:cover; background-position: center;"></div>
+                  <div class="history-actions d-flex flex-row align-items-center justify-content-between">
+                    <div class="history-button opacity-0 visibility-hidden prev-slide-btn">Назад</div>
+                    <div class="history-text text-center">
+                      <p class="mb-0 ml-5">`+v.text+`</p>
+                    </div>
+                    <div class="history-button next-slide-btn" target-slide="`+ nextSlide +`">
+                      <img src="slide btn.png" class="img-fluid slide-btn">
+                    </div>
+                  </div>
+                </div>`
+      }else if (i == json.prestory.length-1) {
+        let prevSlide = i-1;
+        html = `<div class="history-slide" id="history-`+i+`">
+                  <div class="history-image" style="background-image: url('весна.jpg'); background-size:cover; background-position: center;"></div>
+                  <div class="history-actions d-flex flex-row align-items-center justify-content-between">
+                    <div class="history-button prev-slide-btn" target-slide="`+ prevSlide +`">
+                      <img src="slide btn.png" class="img-fluid slide-btn rotate-180">
+                    </div>
+                    <div class="history-text text-center">
+                      <p class="mb-0 mx-5">`+v.text+`</p>
+                    </div>
+                    <div class="history-button next-slide-btn" target-slide="startGame">
+                      <img src="go.png" class="img-fluid slide-btn">
+                    </div>
+                  </div>
+                </div>`
+      }else{
+        let prevSlide = i-1;
+        let nextSlide = i+1;
+        html = `<div class="history-slide" id="history-`+i+`">
+                  <div class="history-image" style="background-image: url('весна.jpg'); background-size:cover; background-position: center;"></div>
+                  <div class="history-actions d-flex flex-row align-items-center justify-content-between">
+                    <div class="history-button prev-slide-btn" target-slide="`+ prevSlide +`">
+                      <img src="slide btn.png" class="img-fluid slide-btn rotate-180">
+                    </div>
+                    <div class="history-text text-center">
+                      <p class="mb-0 mx-5">`+v.text+`</p>
+                    </div>
+                    <div class="history-button next-slide-btn" target-slide="`+ nextSlide +`">
+                      <img src="slide btn.png" class="img-fluid slide-btn">
+                    </div>
+                  </div>
+                </div>`
+      }
+      $('#history-container').append(html);
+    })
+  });
+}
+
+$('body').on('click', '.history-button', function(){
+  let targetSlideNumber = $(this).attr('target-slide');
+  let neededSlide = $('#history-'+targetSlideNumber);
+
+  console.log(neededSlide);
+  $(this).closest('.history-slide').removeClass('active');
+  neededSlide.addClass('active');
+})
